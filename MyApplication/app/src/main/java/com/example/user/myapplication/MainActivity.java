0da4f3546f561963276bb885280a2b5fec769486
tommywey <tommy.wey@gmail.com>
2016-08-09 11:37:46 +0800

    update

--- MyApplication/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -2,21 +2,47 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
+    RadioGroup radioGroup;
+    String drink = "Black Tea";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.txtHelo);
+        editText = (EditText)findViewById(R.id.editText);
+        radioGroup = (RadioGroup)findViewById(R.id.RbG);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+               if (checkedId == R.id.RedTeaBtn)
+               {
+                   drink = "Black Tea";
+               }
+                else
+               {
+                   drink = "Green Tea";
+               }
+            }
+        });
+
     }
 
     public void click(view view)
     {
-        textView.setText("Hi, wey my world");
+        String text= editText.getText().toString();
+        text = text + "Order. " + drink;
+
+        textView.setText(text);
     }
 }
